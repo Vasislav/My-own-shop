80d94cb4cb068896311978bacf957916f4f8c083
Vasislav <fryvasek@seznam.cz>
2024-12-10 08:43:37 +0100

    feat:

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -17,25 +17,34 @@
         </div>
     </div>
 
-    <!-- Why Choose Us Section -->
-    <div class="py-10 text-center bg-gray-100">
+  <!-- Why Choose Us Section -->
+  <div class="py-10 text-center bg-gray-100">
         <h2 class="text-3xl font-semibold">Proč nakupovat u nás?</h2>
         <p class="mt-4 text-lg text-gray-700">Nabízíme nejlepší produkty za nejlepší ceny!</p>
         <div class="mt-6 flex flex-wrap justify-center gap-6">
             <div class="bg-white shadow-md rounded-lg p-6 max-w-xs">
+                <div class="flex justify-center">
+                    <x-heroicon-o-truck class="h-16 w-16" ></x-heroicon-o-truck>
+                </div>
                 <h3 class="text-lg font-bold">Rychlá Doprava</h3>
                 <p class="mt-2">Zaručujeme rychlé dodání vašich objednávek.</p>
             </div>
             <div class="bg-white shadow-md rounded-lg p-6 max-w-xs">
+                <div class="flex justify-center">
+                    <x-iconsax-bro-sidebar-right class="h-16 w-16" ></x-iconsax-bro-sidebar-right>
+                </div>
                 <h3 class="text-lg font-bold">Kvalitní Produkty</h3>
                 <p class="mt-2">Naše produkty procházejí důkladným výběrem kvality.</p>
             </div>
             <div class="bg-white shadow-md rounded-lg p-6 max-w-xs">
+                <div class="flex justify-center">
+                    <x-gmdi-support-agent-o class="h-16 w-16" ></x-gmdi-support-agent-o>
+                </div>
                 <h3 class="text-lg font-bold">Zákaznická Podpora</h3>
                 <p class="mt-2">Jsme tu pro vás, abychom zodpověděli všechny vaše dotazy.</p>
             </div>
         </div>
-    </div>
+    </div> 
 
    <!-- Products Horizontal Scroll Section -->
    <div class="py-10">
@@ -56,8 +65,7 @@
                                         <h3 class="text-lg font-bold text-black">{{ $product->name }}</h3>
                                         <!-- Popis s omezenou výškou, min-height a max-height -->
                                         <p class="mt-2 text-gray-600 overflow-hidden line-clamp-3" style="min-height: 60px; max-height: 120px;">{{ $product->description }}</p>
-                                    </div>
-                                    <div>
+                                    
                                         <p class="mt-2 font-bold">{{ number_format($product->price, 2) }} Kč</p>
                                         <a href="{{ route('products.show', $product->id) }}" class="btn btn-primary inline-block px-6 py-2 text-white bg-blue-500 rounded-md hover:bg-blue-600 transition">Zobrazit detaily</a>
                                     </div>
